2ddb6b2197041580652688d873018b62f7e18cef
bintangprajudha <145175630+bintangprajudha@users.noreply.github.com>
2024-11-29 11:09:19 +0700

    Add files via upload

--- Dokter.h
+++ Dokter.h
@@ -2,7 +2,11 @@
 #define PASIEN_H_INCLUDED
 #include <iostream>
 using namespace std;
-struct dokter {
+
+// adt list parent
+
+struct dokter
+{
     string id;
     string nama;
     string spesialisasi;
@@ -11,22 +15,44 @@ struct dokter {
     int NKD;
     int nomorHP;
 };
+
 typedef struct elmDokter *adr_dokter;
 typedef struct elmRelasi *adr_relasi;
-struct elmDokter {
+typedef struct elmChild *adr_child;
+
+struct elmDokter
+{
     dokter info;
     adr_dokter prev;
     adr_dokter next;
     adr_relasi nextRelasi;
 };
-struct ListDokter {
+
+struct ListDokter
+{
     adr_dokter first;
     adr_dokter last;
 };
+
+// ADT list relasi
+
+struct elmRelasi
+{
+    adr_relasi nextRelasi;
+    adr_child firstChild;
+};
+
+// procedure parent
+
 void createListDokter(ListDokter &LD);
 adr_dokter createElmDokter(dokter D);
 void insertLastDokter(ListDokter &LD, adr_dokter P);
 void DeleteLastDokter(ListDokter &LD, adr_dokter &P);
 void showDokter(ListDokter LD);
 adr_dokter FindDokter(ListDokter LD, string id, string nama, string spesialisasi);
+
+// procedure relasi
+void createListRelasi(elmRelasi &R);
+void insertLastRelasi(ListDokter &LD, adr_relasi R);
+void DeleteFirstRelasi(ListDokter &LD, adr_relasi &R);
 #endif // PASIEN_H_INCLUDED
