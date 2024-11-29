de807ef1b1aba9895127c1175ca47e83f63f3d13
gazel29 <gazelakbari@gmail.com>
2024-11-29 08:55:11 +0700

    Create Dokter.h

---
+++ Dokter.h
@@ -0,0 +1,32 @@
+#ifndef PASIEN_H_INCLUDED
+#define PASIEN_H_INCLUDED
+#include <iostream>
+using namespace std;
+struct dokter {
+    string id;
+    string nama;
+    string spesialisasi;
+    string jk;
+    int umur;
+    int NKD;
+    int nomorHP;
+};
+typedef struct elmDokter *adr_dokter;
+typedef struct elmRelasi *adr_relasi;
+struct elmDokter {
+    dokter info;
+    adr_dokter prev;
+    adr_dokter next;
+    adr_relasi nextRelasi;
+};
+struct ListDokter {
+    adr_dokter first;
+    adr_dokter last;
+};
+void createListDokter(ListDokter &LD);
+adr_dokter createElmDokter(dokter D);
+void insertLastDokter(ListDokter &L, adr_dokter P);
+void DeleteLastDokter(ListDokter &L, adr_dokter &P);
+void showDokter(ListDokter LD);
+adr_dokter FindDokter(ListDokter LD, string id, string nama, string spesialisasi);
+#endif // PASIEN_H_INCLUDED
