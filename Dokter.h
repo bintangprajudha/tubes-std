66b0a5162576cfb18b60f7f8d07cd376821534c6
Aditalbqhr <161371847+Aditalbqhr@users.noreply.github.com>
2024-11-29 11:38:10 +0700

    Add files via upload

--- Dokter.h
+++ Dokter.h
@@ -1,5 +1,5 @@
-#ifndef PASIEN_H_INCLUDED
-#define PASIEN_H_INCLUDED
+#ifndef DOKTER_H_INCLUDED
+#define DOKTER_H_INCLUDED
 #include <iostream>
 using namespace std;
 
@@ -18,7 +18,7 @@ struct dokter
 
 typedef struct elmDokter *adr_dokter;
 typedef struct elmRelasi *adr_relasi;
-typedef struct elmChild *adr_child;
+typedef struct elmChild *adr_pasien;
 
 struct elmDokter
 {
@@ -33,13 +33,35 @@ struct ListDokter
     adr_dokter first;
     adr_dokter last;
 };
+// ADT list pasien
 
+struct Pasien{
+    string Nama;
+    string NIK;
+    string JenisKelamin;
+    string golonganDarah;
+    string berat;
+    string tinggiBadan;
+    string TTL;
+    int usia;
+    bool asuransiKesehatan;
+    string agama;
+};
+
+struct elmPasien{
+    Pasien info;
+    adr_pasien next;
+};
+
+struct ListPasien{
+    adr_pasien first;
+};
 // ADT list relasi
 
 struct elmRelasi
 {
     adr_relasi nextRelasi;
-    adr_child firstChild;
+    adr_pasien firstChild;
 };
 
 // procedure parent
@@ -55,4 +77,6 @@ adr_dokter FindDokter(ListDokter LD, string id, string nama, string spesialisasi
 void createListRelasi(elmRelasi &R);
 void insertLastRelasi(ListDokter &LD, adr_relasi R);
 void DeleteFirstRelasi(ListDokter &LD, adr_relasi &R);
-#endif // PASIEN_H_INCLUDED
+
+
+#endif // DOKTER_H_INCLUDED
