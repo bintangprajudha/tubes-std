9452e05881d43ec612df0f6a435f20c0b105e896
bintangprajudha <145175630+bintangprajudha@users.noreply.github.com>
2024-12-06 23:47:03 +0700

    Update header.h

--- header.h
+++ header.h
@@ -82,7 +82,7 @@ int hitungPasienDariDokter(ListDokter LD, string id_dokter, string nama_dokter);
 void createListPasien(ListPasien &LP);
 adr_pasien createElmPasien(Pasien C);
 void insertLastPasien(ListPasien &LP, adr_pasien P);
-void deleteFirstPasien(ListPasien &LP, adr_pasien &P);
+void deleteFirstPasien(ListPasien &LP, ListDokter &LD, adr_pasien &P);
 adr_pasien FindPasien(ListPasien LP, string NIK, string nama);
 void ShowPasien(ListPasien LP);
 void hitungRelasiPasien(ListDokter LD, ListPasien LP, int &jumRelasi);
@@ -98,6 +98,14 @@ void showChildParent(ListPasien LP, ListDokter LD);
 adr_relasi deleteAfterRelasi(adr_dokter &dokter, adr_pasien pasienLama);
 void editRelasiGantiPasien(ListDokter &LD, ListPasien &LP, string id_dokter, string nama_dokter, string nik_pasien, string nama_pasien_lama, string new_nik_pasien, string nama_pasien_baru);
 
+
+// bagian menu
 int menu();
+void inputDataDokter(dokter &D);
+void inputDataPasien(Pasien &P);
+int case6();
+int case7();
+int case8();
+int case9();
 
 #endif // HEADER_H_INCLUDED
